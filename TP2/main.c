bc2ef62b88fd294da4bbbdd1bd50596f827aea60
Matías Ojea <mojea@frba.utn.edu.ar>
2024-09-04 17:11:51 -0300

    actualizo main.c

--- TP2/main.c
+++ TP2/main.c
@@ -1,4 +1,18 @@
+#include "scanner.h"
 
+FILE* archivo;
 
+int main()
+{
+    archivo = fopen("../TP2/output/entrada.txt", "r");
+    char* token = NULL;
 
+    token = obtenerToken(); 
+    // do{
+    //    token = obtenerToken();
+    // } while (token != "FDT");
+    // Se usa a futuro
 
+    fclose(archivo);
+    return 0;
+}
