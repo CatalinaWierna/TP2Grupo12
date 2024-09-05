9137fb5affc926166b1d17db4db960eaf630563f
arolla03 <pintosrollaalvaro@gmail.com>
2024-09-05 02:00:29 -0300

    main.c

--- TP2/main.c
+++ TP2/main.c
@@ -1,4 +1,27 @@
+#include "scanner.h"
 
+FILE* archivo;
+
+int main()
+{
+    archivo = fopen("../TP2/output/entrada.txt", "r");
+    char* token = NULL;
+
+    if (archivo == NULL){
+        printf("Contenido vacio en el archivo.\n");
+        fclose(archivo);
+        return 1;
+    }
+
+    token = obtenerToken(); 
+    do{
+        token = obtenerToken();
+    } while (token != EOF);
+
+
+    fclose(archivo);
+    return 0;
+}
 
 
 
